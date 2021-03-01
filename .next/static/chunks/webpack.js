5d99c9259e3bda09a0e9ca8ce3d92b29d0a8e412
younheeJang <edam7635@gmail.com>
2021-03-01 16:13:05 +0900

    update static generation's head

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a0df5b6bbc39e54bcbbb";
+/******/ 	var hotCurrentHash = "d3fc64bca767c9874ae5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
