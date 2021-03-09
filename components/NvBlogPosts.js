0a1ae88ae278d7320e48113f073dcd7936ab5b73
younheeJang <edam7635@gmail.com>
2021-03-10 00:48:50 +0900

    Worked: NvBlogPosts with ustSWR

--- components/NvBlogPosts.js
+++ components/NvBlogPosts.js
@@ -1,40 +1,34 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useRouter } from "next/router";
-import { resolve } from 'q';
+import useSWR, { mutate } from 'swr'
 
 const URL = "/api/posts";
 
 export default function NvBlogPosts(){
-    const router = useRouter(); 
+ 
+    const [searchingWord, setInputValue] = useState("");
+
+    function handleInputChange(event) {
+        setInputValue(event.target.value);
+    }
+
+
+    const fetcher = (...args) => fetch(...args).then(res => res.json()).then(data => data.items)
+    const { data:posts } = useSWR(searchingWord ? `${URL}/${searchingWord}`: null, fetcher)
+   
     
-    const { 
-        query: { searchingWord }, 
-    } = router; 
-    
-    let posts = null
-    let textInput = React.createRef();
     
     async function fetchPostsData() {
-        console.log(posts)
-        const searchingWord = textInput.current.value;
-        console.log(searchingWord)
-        console.log(`${URL}/${searchingWord}`)
-        let result = await fetch(`${URL}/${searchingWord}`).then(res => res.json())//.then(res => res.json()).catch(err => console.log(err))
-        posts = result.items
-        console.log(posts)
+        mutate(`${URL}/${searchingWord}`, false)  
     }
     
     if(!posts){
     return (
         <div className="overflow-y-auto min-w-0 flex-auto px-4 sm:px-6 xl:px-8 pt-10 pb-24 lg:pb-16">
-        <div className="relative overflow-hidden text-gray-600 max-w-md mx-auto bg-white rounded-xl shadow-md  md:max-w-2xl mb-5">
-            <input ref={textInput} type="search" name="search" placeholder="no data" className="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none" />
-            <button type="submit" className="absolute right-0 top-0 mt-3 mr-4" onClick={fetchPostsData}>
-                <Image width={15} height={15} src="/images/search-interface-symbol.png" className="h-4 w-4 fill-curren"></Image>
-            </button>
-        </div>
+            <div className="text-gray-600 max-w-md mx-auto bg-white rounded-xl shadow-md  md:max-w-2xl mb-5">
+                <input value={ searchingWord } onChange={ handleInputChange } onMouseUp={fetchPostsData} type="search" name="search" placeholder="Search" className="bg-white h-10 px-5 m-3 pr-10 pl-10 rounded-full text-sm focus:outline-none" />
+            </div>
         </div>
     ) 
     } 
@@ -42,29 +36,24 @@ export default function NvBlogPosts(){
     if(posts){
     return (
         <div className="overflow-y-auto min-w-0 flex-auto px-4 sm:px-6 xl:px-8 pt-10 pb-24 lg:pb-16">
-        <div className="relative overflow-hidden text-gray-600 max-w-md mx-auto bg-white rounded-xl shadow-md  md:max-w-2xl mb-5">
+            <div className="text-gray-600 max-w-md mx-auto bg-white rounded-xl shadow-md  md:max-w-2xl mb-20">
+                <input value={ searchingWord } onChange={ handleInputChange } onMouseUp={fetchPostsData} type="search" name="search" placeholder="Search" className="overflow-hidden bg-white h-10 px-5 m-3 pr-10 pl-10 rounded-full text-sm focus:outline-none" />     
+            </div>
         
-            <input ref={textInput} type="search" name="search" placeholder="Search" className="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none" />
-            <button type="submit" className="absolute right-0 top-0 mt-3 mr-4" onClick={fetchPostsData}>
-                <Image width={15} height={15} src="/images/search-interface-symbol.png" className="h-4 w-4 fill-curren"></Image>
-            </button>
-        
-        </div>
-    
-      {posts && posts.map(({ link, title, postdate, bloggername, description }) => (
-        <Link href={link} key={link} className="cursor-pointer">
-            <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mb-5">
-                <div className="md:flex">
-                    <div className="p-8">
-                        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{title.replace(/<[^>]*>?/gm, '')}</div>
-                        <a href="#" className="block mt-1 text-xs leading-tight font-medium text-black text-right p-1">Wriiten by '{bloggername}', &nbsp; &nbsp;Posted at {postdate}</a>
-                        <p className="mt-2 text-gray-500">{description.replace(/<[^>]*>?/gm, '')}</p>
+            {posts && posts.map(({ link, title, postdate, bloggername, description }) => (
+                <Link href={link} key={link} className="cursor-pointer">
+                    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md md:max-w-2xl mb-5">
+                        <div className="md:flex">
+                         <div className="p-8">
+                            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{title.replace(/<[^>]*>?/gm, '')}</div>
+                            <a href="#" className="block mt-1 text-xs leading-tight font-medium text-black text-right p-1">Wriiten by '{bloggername}', &nbsp; &nbsp;Posted at {postdate}</a>
+                            <p className="mt-2 text-gray-500">{description.replace(/<[^>]*>?/gm, '')}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </Link>
-      ))}  
-    </div>
+            </Link>
+            ))}  
+        </div>
     )
   }
 }
