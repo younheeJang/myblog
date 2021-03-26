66a3fcdb7add26b54299b22eff1a9c3f8ccb16b7
younheeJang <edam7635@gmail.com>
2021-03-26 23:53:15 +0900

    Worked: /api/kko/pay/ready

--- components/kko/ShoppingItems.tsx
+++ components/kko/ShoppingItems.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 interface ShoppingItemProps {
   Id: string;
@@ -10,10 +11,37 @@ interface ShoppingItemProps {
 
 const ShoppingItems: React.FC<ShoppingItemProps[]> = () => {
 
-  const KakaoPayReady = () => {
+  const KakaoPayReady = async () => {
+    console.log('started')
+    await fetch('/api/kko/pay')
+  }
+  const baseURL = 'https://kapi.kakao.com';
+  const KakaoPayReady1 = async () => {
+    const data = {
+      cid:'TC0ONETIME',
+      partner_order_id: '1234',
+      partner_user_id: 'jeager',
+      item_name: 'flower',
+      quantity: 100,
+      total_amount: 1640000,
+      tax_free_amount: 0,
+      approval_url:'http:localhost:3000/shop/items',
+      cancel_url: 'http:localhost:3000',
+      fail_url: 'http:localhost:3000',
+  };
+    const kko_admin_key = process.env.kko_admin_key;
+    const headers = {
+      'Authorization': 'KakaoAK '+`${kko_admin_key}`,
+      'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+      mode: 'no-cors',
+    }
+    axios.defaults.headers.post = null
+     await axios.post(`${baseURL}/v1/payment/ready`, data, {headers})
+    .then(res => {
+      console.log('send ok', res.data)
+    }).catch(err => console.log(err))
   
   }
-  
 const items : ShoppingItemProps[]= [
   {
     Id: 'qw123',
@@ -37,7 +65,8 @@ const items : ShoppingItemProps[]= [
      <div key={Id} className="cursor-pointer lg:w-1/4 md:w-1/2 sm:w-full p-4">
         <a className="block h-60 ">
           <img alt="product-flower1" className=" rounded object-cover object-center w-full h-full block" src={Image} />
-          <img alt='payment' src='/images/payment_icon_yellow_small.png' className=' h-5 w-10 lg:-my-10 lg:mx-40 md:-my-10 md:mx-72 -my-10 ml-3'/>     
+          <img alt='payment' src='/images/payment_icon_yellow_small.png' 
+               className=' h-5 w-10 lg:-my-10 lg:mx-40 md:-my-10 md:mx-72 -my-10 ml-3' onClick={KakaoPayReady}/>     
         </a>
         <div className="mt-4">
           <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{Category}</h3>
--- pages/api/kko/pay.tsx
+++ pages/api/kko/pay.tsx
@@ -1,6 +1,8 @@
 import Cors from 'cors'
 import initMiddleware from '../../../lib/init-middleware'
 import { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios'
+import ObjectToFormData from '../../../lib/utils/object-to-formdata'
 
 export const config = {
   api: {
@@ -14,11 +16,37 @@ const cors = initMiddleware(
   })
 )
 
+const baseURL = 'https://kapi.kakao.com';
+
 export default async function handler(req:NextApiRequest , res:NextApiResponse) {
   // Run cors
   await cors(req, res)
+  
+  const data = {
+    'cid':'TC0ONETIME',
+    'partner_order_id': 'curious',
+    'partner_user_id': 'jeager',
+    'item_name': 'flower',
+    'quantity': 1,
+    'total_amount': 10,
+    'tax_free_amount': 0,
+    'approval_url': 'http://localhost:3000',
+    'fail_url': 'http://localhost:3000',
+    'cancel_url': 'http://localhost:3000',
+};
 
+  const FormData = ObjectToFormData(data)
   const kko_admin_key = process.env.kko_admin_key;
+  
+  const headers = {
+    'Authorization': 'KakaoAK '+`${kko_admin_key}`,
+    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+  }
+  axios.defaults.headers.post = null
+   await axios.post(`${baseURL}/v1/payment/ready`, FormData, {headers})
+  .then(res => {
+    console.log('send ok', res.data)
+  }).catch(err => console.log(err))
   
   return
 }
