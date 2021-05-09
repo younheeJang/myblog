ce886dff85237f62d862b4bfa511d79cccd1cd8b
younheeJang <edam7635@gmail.com>
2021-05-10 01:25:10 +0900

    Setting: basic box text on game/Box component

--- components/game/Box.tsx
+++ components/game/Box.tsx
@@ -1,11 +1,53 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef } from "react";
+import { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse, Body, Events } from "matter-js";
 
-
-const Box: React.FC = () => {
-  
+const Box: React.FC = (props) => {
+    const box = useRef();
+    const engine = useRef(Engine.create())
+    useEffect(() => {
+        // mount
+        const cw = document.body.clientWidth
+        const ch = document.body.clientHeight
+      
+        const render = Render.create({
+          element: box.current,
+          engine: engine.current,
+          options: {
+            width: cw,
+            height: ch,
+            wireframes: false,
+            background: 'transparent'
+          }
+        })
+          
+        // boundaries
+        World.add(engine.current.world, [
+          Bodies.rectangle(cw / 2, -10, cw, 20, { isStatic: true }),
+          Bodies.rectangle(-10, ch / 2, 20, ch, { isStatic: true }),
+          Bodies.rectangle(cw / 2, ch + 10, cw, 20, { isStatic: true }),
+          Bodies.rectangle(cw + 10, ch / 2, 20, ch, { isStatic: true })
+        ])
+          
+        // run the engine
+        Engine.run(engine.current)
+        Render.run(render)
+      
+        // unmount
+        return () => {
+          // destroy Matter
+          Render.stop(render)
+          World.clear(engine.current.world)
+          Engine.clear(engine.current)
+          render.canvas.remove()
+          render.canvas = null
+          render.context = null
+          render.textures = {}
+        }
+      }, [])
+    
   return (
     <>
-    
+        <div ref={box} />;
     </>
   );
 };
--- components/game/Header.tsx
+++ components/game/Header.tsx
@@ -1,11 +1,26 @@
 import React, { useState } from "react";
 
-
 const Header: React.FC = () => {
-  
   return (
     <>
-    
+      <nav className="flex items-center justify-between flex-wrap bg-teal p-6 bg-red-300">
+        <div className="flex items-center flex-no-shrink text-white mr-6">
+          <img className='m-1 w-10 h-8' src='/images/LOGO.png'/>
+          <span className="font-semibold text-xl tracking-tight">Curious Jeager</span>
+        </div>
+        <div className="block lg:hidden">
+          <button className="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
+            <svg
+              className="h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+      </nav>
     </>
   );
 };
